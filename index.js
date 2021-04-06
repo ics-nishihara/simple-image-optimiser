211889dc878498046e3c468a39f2bf9b5f886cc8
ics-nishihara <nishihara@ics-web.jp>
2021-04-07 00:40:34 +0900

    拡張子によるファイルの取得

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ const path = require("path");
 const util = require("util");
 const execPromise = util.promisify(exec);
 
-const regex = new RegExp(".+\.(jpe?g|png)","g")
+// console.log(process.argv[2])
+
+const imagesData = { jpg: [], png: [] };
+
+const regex = new RegExp("([^s]+(\\.(jpe?g|png))$)", "i");
 const imagePath = path.resolve(__dirname, "images");
 const allDirents = fs.readdirSync(imagePath, { withFileTypes: true });
-const filePathList = allDirents
-  .filter((dirent) => dirent.isFile())
-  .filter(dirent=> regex.test(dirent.name))
-  .map((dirent) => `images/${dirent.name}`).join(" ");
-// console.log(filePathList);
-console.log(`yarn run squoosh-cli --webp '{quality: 75}' ${filePathList} -d dist`)
-execPromise(`yarn run squoosh-cli --webp '{quality: 75}' ${filePathList} -d dist`)
-// exec("yarn run convert-jpg-to-webp");
+allDirents
+  .filter((dirent) => dirent.isFile() && regex.test(dirent.name))
+  .forEach((dirent) => {
+    const ext = path.extname(dirent.name);
+    if (ext.match(/.jpe?g/i)) {
+      imagesData.jpg.push(`images/${dirent.name}`);
+    }
+    if (ext.match(/.png/i)) {
+      imagesData.png.push(`images/${dirent.name}`);
+    }
+  });
+
+const allFilePathList = [...imagesData.jpg,...imagesData.png].join(" ")
+const jpgFilePathList = imagesData.jpg.join(" ")
+const pngFilePathList = imagesData.png.join(" ")
+console.log(pngFilePathList);
+// console.log(`yarn run squoosh-cli --webp '{quality: 75}' ${filePathList} -d dist`)
+execPromise(
+  `yarn run squoosh-cli --webp '{quality: 75}' ${allFilePathList} -d dist`
+);
 process.exit();
